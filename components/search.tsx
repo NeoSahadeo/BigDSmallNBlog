710e59030adac854916e5d59f1964897548f6048
NeoSahadeo <neosahadeo@protonmail.com>
2024-01-22 22:19:09 +0000

    fix: rewrote search using regex to make more extensible

--- components/search.tsx
+++ components/search.tsx
@@ -5,25 +5,27 @@ import { Post } from '@/utils/marky';
 import { Search as SearchSVG } from 'lucide-react';
 
 const search = (e:BaseSyntheticEvent, posts: Post[]) => {
-  /* Damian Fixed. 
+  /* Damian Fixed uwu. 
    *
    * @return
    * posts array that has been filtered based on {title, description, date}
    * */
-  const value = e.target.value.toUpperCase()
-  return posts.filter(post => (
-    post.title.toUpperCase().includes(value) ||
-    post.description.toUpperCase().includes(value) ||
-    post.date.toUpperCase().includes(value)
-  ))
+  const value = new RegExp( e.target.value, 'gi')
+  return posts.filter(post => {
+    return (
+    post.title.match(value) || 
+    post.description.match(value) ||
+    post.date.match(value) ||
+    post.categories.some(element => element.match(value))
+    )
+  })
 }
 
 export default function Search({posts, setPostsToDisplay}: {posts: Post[], setPostsToDisplay: any}) {
   return(
   <span className="flex flex-col sm:flex-row sm:items-center border px-2 py-1 rounded-sm border-gray-400">
-    <label style={{textWrap: 'nowrap'}} className='w-min'>Search blogs: </label>
     <span className='flex flex-row w-full items-center'>
-    <input className={`${styles.input} dark:bg-transparent`} onInput={(e)=>setPostsToDisplay(search(e, posts))}></input>
+    <input className={`${styles.input} dark:bg-transparent`} onInput={(e)=>setPostsToDisplay(search(e, posts))} placeholder='Search Blogs'></input>
     <SearchSVG />
     </span>
   </span>
