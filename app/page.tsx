0561a1e771f03297db83a3a6774fc930fa1e1f07
NeoSahadeo <neosahadeo@protonmail.com>
2024-01-22 18:18:26 +0000

    fix: Removed posts export from app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import GetSortedPosts from './utils/marky';
 import Home from './home';
 
-export const posts = GetSortedPosts()
+const posts = GetSortedPosts()
 
 export default function Page() {
   return <Home posts={posts}/>
