59cf0ddb13349a3d3af1a55bd03b6ea90765a4a0
damiandare <damian.dare@madsoftware.co.za>
2024-01-24 14:42:50 +0200

    refactor: change page layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,38 +1,39 @@
-import type { Metadata } from "next";
-import { Zilla_Slab } from 'next/font/google'
-import { Roboto_Mono } from 'next/font/google'
-import "./globals.css";
-import Header from "@/components/header";
-import Footer from "@/components/footer";
+import type { Metadata } from "next"
+import { Zilla_Slab } from "next/font/google"
+import { Roboto_Mono } from "next/font/google"
+import "./globals.css"
+import Header from "@/components/header"
+import Footer from "@/components/footer"
 import { ThemeProvider } from "@/components/theme-provider"
 
 const zilla_slab = Zilla_Slab({
-  weight: '300', 
-  subsets: ['latin']
+  weight: "300",
+  subsets: ["latin"],
 })
 const roboto_mono = Roboto_Mono({
-  subsets: ['latin'],
-  variable: '--font-roboto-mono'
+  subsets: ["latin"],
+  variable: "--font-roboto-mono",
 })
 
 export const metadata: Metadata = {
   title: "Big D Small N",
   description: "Damian and Neo's Blog site.",
-};
+}
 
-export default function RootLayout({ children }: Readonly<{ children: React.ReactNode; }>) { 
-return (
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
     <html lang="en">
       <body className={`${zilla_slab.className} dark:text-gray-300`}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-        >
-          <Header/>
-          {children}
-          <Footer/> 
+        <ThemeProvider attribute="class" defaultTheme="system">
+          <div className="flex flex-col min-h-screen gap-5">
+            <Header />
+            <div className="grow">{children}</div>
+            <Footer />
+          </div>
         </ThemeProvider>
       </body>
     </html>
-  );
+  )
 }
