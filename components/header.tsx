bb29072dbcdf3ce41e0d8fd2eeb796074055101b
damiandare <damian.dare@madsoftware.co.za>
2024-01-25 10:28:56 +0200

    feat: add logo to header component

--- components/header.tsx
+++ components/header.tsx
@@ -3,11 +3,20 @@
 import Link from "next/link"
 import { ThemeToggler } from "./theme-toggler"
 import { Button } from "./ui/button"
+import Image from "next/image"
+import dnaLogo from "@/public/dna_logo.svg"
 
 export default function Header() {
   return (
     <header className="flex p-5 gap-5 items-center">
-      <p>Insert Logo</p>
+      {/* <p>Insert Logo</p> */}
+      <Image
+        src={dnaLogo}
+        // width={300}
+        // height={300}
+        className="w-16 aspect-square"
+        alt="dna-logo"
+      />
       <Button variant="link" className="text-lg font-bold ml-auto" asChild>
         <Link href="/">Home</Link>
       </Button>
