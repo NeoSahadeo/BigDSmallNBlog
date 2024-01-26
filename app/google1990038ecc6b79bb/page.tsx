2841a55175923c487de15c8d663d0cdda9107c4d
NeoSahadeo <neosahadeo@protonmail.com>
2024-01-26 14:29:56 +0000

    fix: switch to meta tag verification google

--- app/google1990038ecc6b79bb/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function Page () {
-  return <>
-    google-site-verification: google1990038ecc6b79bb.html
-  </>
-}
--- app/home.tsx
+++ app/home.tsx
@@ -10,6 +10,7 @@ export default function Home({posts}: {posts: Post[]}) {
   const [postsToDisplay, setPostsToDisplay] = useState(posts)
     return (
         <main className='pl-4 pr-4 sm:pl-0 sm:pr-0'>
+        <meta name="google-site-verification" content="5pK2iYStjM71NhXfkntNYdRfI6rCZ99T2kdKe1pGuxM" />
         <section>
         <h1 className='title'>Big D and Small N&apos;s Blog</h1>
         <Search posts={posts} setPostsToDisplay={setPostsToDisplay}/>
