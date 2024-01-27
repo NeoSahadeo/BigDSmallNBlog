5c0cd62d79f4b07a129da3792be287d2a120b77a
NeoSahadeo <neosahadeo@protonmail.com>
2024-01-27 18:25:46 +0000

    fix: made fish use a url instead of public dir

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,13 +6,14 @@ export default async function Custom404() {
       <h1>404 - Page Not Found</h1>
       <p>What are you looking for?</p>
       <article>
-        <Image
+        <img src='https://i.pinimg.com/736x/f8/4b/d0/f84bd082d1e31fa54d93af385b759be5.jpg' width={300} height={100} className='w-full rounded' alt='Fish Meme'></img>
+        {/* <Image
           src="/fish404.jpg"
           width={300}
           height={100}
           alt='Fish meme'
           className='w-full rounded'
-        ></Image>
+        ></Image> */}
       </article>
     </main>
   )
