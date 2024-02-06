926414909c93ed772196c6a4370f9f06fcdca539
damiandare <damian.dare@madsoftware.co.za>
2024-02-06 09:45:41 +0200

    refactor: no clue what happened here ... its been a while sorry :(

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -35,8 +35,8 @@ export default async function BlogPage({ params }: PageProps) {
 
   return (
     <div className="flex flex-col items-center px-4 grow gap-7 ">
-      <ScrollArea className="px-5">
-        <section className="px-2 sm:px-0 flex flex-col gap-1">
+      <ScrollArea className="px-5 text-lg">
+        {/* <section className="px-2 sm:px-0 flex flex-col gap-1">
           <h1 className="text-5xl font-bold">{metadata.title}</h1>
           <p style={{ marginBottom: "0.5rem" }}>
             <i>{metadata.description}</i>
@@ -44,26 +44,54 @@ export default async function BlogPage({ params }: PageProps) {
           <p>{metadata.author}</p>
           <p style={{ marginBottom: "1rem" }}>{metadata.date}</p>
           <hr className="bg-gray-300"></hr>
-          {/* <article
-            dangerouslySetInnerHTML={{ __html: parsedFileContents }}
-          ></article> */}
-          {/* <article>{parsedFileContents}</article> */}
-        </section>
+        </section> */}
 
         <Markdown
-        // options={{
-        //   overrides: {
-        //     p: {
-        //       props: {
-        //         className: "text-blue-500",
-        //       },
-        //     },
-        //   },
-        // }}
+          options={{
+            overrides: mdStyles,
+          }}
         >
           {parsedFileContents as string}
         </Markdown>
       </ScrollArea>
     </div>
   )
+}
+
+const mdStyles = {
+  p: {
+    props: {
+      className: "",
+    },
+  },
+  h1: {
+    props: {
+      className: "text-5xl font-bold",
+    },
+  },
+  h2: {
+    props: {
+      className: "text-4xl font-bold",
+    },
+  },
+  h3: {
+    props: {
+      className: "text-3xl font-bold",
+    },
+  },
+  h4: {
+    props: {
+      className: "text-2xl font-bold",
+    },
+  },
+  h5: {
+    props: {
+      className: "text-xl font-bold",
+    },
+  },
+  h6: {
+    props: {
+      className: "text-lg font-bold",
+    },
+  },
 }
--- hooks/useGetFile.ts
+++
@@ -1,8 +0,0 @@
-import { postsDirectory } from "@/utils/marky"
-import matter from "gray-matter"
-import fs from "fs"
-import path from "path"
-
-export default function useGetFile(fileId: string) {
-  const filePath = path.join(postsDirectory, fileId) + ".md"
-}
